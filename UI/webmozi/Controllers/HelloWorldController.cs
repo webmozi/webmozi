ae3e701e39b61de53b06e910b5ad3affa3dd9b44
akob4ko <djch3mo@hotmail.com>
2018-09-23 15:03:41 +0200

    Ákó projekt

---
+++ UI/webmozi/Controllers/HelloWorldController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+using System.Web;
+
+namespace webmozi.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+      
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        //GET: /HelloWorld/Welcome/
+        public ActionResult Welcome(string name, int numTimes = 1) {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+    }
+}
